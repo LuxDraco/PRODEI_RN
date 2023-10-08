30497737e666e8507e12ffe8e1228a6ecc732246
Jose Alfonso Gomez Coeto <ponchogcja@gmail.com>
2023-10-08 16:02:13 -0600

    Subida de archivos iniciales

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import {Button, PaperProvider} from 'react-native-paper';
+import {AppTheme} from "./Utils/Theming";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <PaperProvider theme={AppTheme}>
+            <View style={styles.container}>
+                <Text>Open up App.js to start working on your app!</Text>
+                <StatusBar style="auto"/>
+                <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+                    Press me
+                </Button>
+            </View>
+        </PaperProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
---
+++ Utils/Theming.js
@@ -0,0 +1,10 @@
+import {DefaultTheme} from "react-native-paper";
+
+export const AppTheme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: '#3498db',
+        secondary: '#f1c40f',
+    }
+};
